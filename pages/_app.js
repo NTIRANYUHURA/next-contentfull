569315a2e17a950af7b77e5979a9aa05af912ed2
NTIRANYUHURA <floribertnt@gmail.com>
2022-09-06 23:12:04 +0200

    tailwindcss and navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
+
+import Navbar from '../components/Navbar'
+import Main from '../pages/Main'
+
 import '../styles/globals.css'
+import Gallery from '../Pages/gallery';
+import Contact from '../Pages/contact';
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <>
+  <Navbar/>
+  <Main/>
+  <Gallery/>
+ 
+  <Contact/>
+  <Component {...pageProps} />
+  </>
+  )
 }
 
 export default MyApp
